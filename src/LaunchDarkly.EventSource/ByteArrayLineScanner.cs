77ebf56b32045d127ebb63ff8fd9af45c73775bb
Eli Bishop <eli@launchdarkly.com>
2021-01-29 11:57:18 -0800

    comments

--- src/LaunchDarkly.EventSource/ByteArrayLineScanner.cs
+++ src/LaunchDarkly.EventSource/ByteArrayLineScanner.cs
@@ -42,11 +42,26 @@ namespace LaunchDarkly.EventSource
             _startPos = 0;
         }
 
+        /// <summary>
+        /// The caller calls this method after having already added <c>count</c> more bytes at the
+        /// end of the buffer. We do it this way instead of having an <c>AddBytes(byte[], int)</c>
+        /// method because we don't want to allocate a second buffer just to be the destination for
+        /// a read operation.
+        /// </summary>
+        /// <param name="count">number of bytes added</param>
         public void AddedBytes(int count)
         {
             _count += count;
         }
 
+        /// <summary>
+        /// Searches for the next line ending and, if successful, provides the line data.
+        /// </summary>
+        /// <param name="lineOut">if successful, this is set to point to the bytes for the line
+        /// <i>not</i> including any CR/LF; whenever possible this is a reference to the underlying
+        /// buffer, not a copy, so the caller should read/copy it before doing anything else to the
+        /// buffer</param>
+        /// <returns>true if a full line was read, false if we need more data first</returns>
         public bool ScanToEndOfLine(out Utf8ByteSpan lineOut)
         {
             if (_startPos == _count)
@@ -135,7 +150,9 @@ namespace LaunchDarkly.EventSource
                 lineOut = new Utf8ByteSpan(_partialLine.GetBuffer(), 0, (int)_partialLine.Position);
                 _partialLine = null;
 
-                // If there are still bytes in the buffer, move them over to make more room
+                // If there are still bytes in the main buffer, move them over to make more room. It's
+                // safe for us to do this before the caller has looked at lineOut, because lineOut is now
+                // a reference to the separate _partialLine buffer, not to the main buffer.
                 if (_startPos < _count)
                 {
                     System.Buffer.BlockCopy(_buffer, _startPos, _buffer, 0, _count - _startPos);
@@ -148,7 +165,12 @@ namespace LaunchDarkly.EventSource
                 lineOut = new Utf8ByteSpan(_buffer, startedAt, pos - startedAt);
                 if (_startPos == _count)
                 {
-                    _startPos = _count = 0; // reset for the next read
+                    // If we've scanned all the data in the buffer, reset _startPos and _count to indicate
+                    // that the entire buffer is available for the next read. It's safe for us to do this
+                    // before the caller has looked at lineOut, because we're not actually modifying any
+                    // bytes in the buffer. It's the caller's not responsibility not to modify the buffer
+                    // until it has already done whatever needs to be done with the lineOut data.
+                    _startPos = _count = 0;
                 }
             }
             return true;
