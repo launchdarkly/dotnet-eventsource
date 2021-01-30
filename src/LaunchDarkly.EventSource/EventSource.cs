feeb7440fa2d86114f6703ab3c2cc054dc3271e5
Eli Bishop <eli@launchdarkly.com>
2021-01-29 18:29:07 -0800

    better doc comments, formatting

--- src/LaunchDarkly.EventSource/EventSource.cs
+++ src/LaunchDarkly.EventSource/EventSource.cs
@@ -34,37 +34,26 @@ namespace LaunchDarkly.EventSource
 
         #region Public Events
 
-        /// <summary>
-        /// Occurs when the connection to the EventSource API has been opened.
-        /// </summary>
+        /// <inheritdoc/>
         public event EventHandler<StateChangedEventArgs> Opened;
-        /// <summary>
-        /// Occurs when the connection to the EventSource API has been closed.
-        /// </summary>
+
+        /// <inheritdoc/>
         public event EventHandler<StateChangedEventArgs> Closed;
-        /// <summary>
-        /// Occurs when a Server Sent Event from the EventSource API has been received.
-        /// </summary>
+
+        /// <inheritdoc/>
         public event EventHandler<MessageReceivedEventArgs> MessageReceived;
-        /// <summary>
-        /// Occurs when a comment has been received from the EventSource API.
-        /// </summary>
+
+        /// <inheritdoc/>
         public event EventHandler<CommentReceivedEventArgs> CommentReceived;
-        /// <summary>
-        /// Occurs when an error has happened when the EventSource is open and processing Server Sent Events.
-        /// </summary>
+
+        /// <inheritdoc/>
         public event EventHandler<ExceptionEventArgs> Error;
 
         #endregion Public Events
 
         #region Public Properties
 
-        /// <summary>
-        /// Gets the state of the EventSource connection.
-        /// </summary>
-        /// <value>
-        /// One of the <see cref="EventSource.ReadyState"/> values, which represents the state of the EventSource connection.
-        /// </value>
+        /// <inheritdoc/>
         public ReadyState ReadyState
         {
             get
@@ -124,13 +113,7 @@ namespace LaunchDarkly.EventSource
 
         #region Public Methods
 
-        /// <summary>
-        /// Initiates a connection to the SSE server and begins parsing events.
-        /// </summary>
-        /// <returns>a <see cref="Task"/> that will be completed only when the
-        /// <c>EventSource</c> is closed</returns>
-        /// <exception cref="InvalidOperationException">if the method was called again after the
-        /// stream connection was already active</exception>
+        /// <inheritdoc/>
         public async Task StartAsync()
         {
             bool firstTime = true;
@@ -227,18 +210,7 @@ namespace LaunchDarkly.EventSource
             }
         }
 
-        /// <summary>
-        /// Triggers the same "close and retry" behavior as if an error had been encountered on the stream.
-        /// </summary>
-        /// <remarks>
-        /// If the stream is currently active, this closes the connection, waits for some amount of time
-        /// as determined by the usual backoff behavior (and <paramref name="resetBackoffDelay"/>), and
-        /// then attempts to reconnect. If the stream is not yet connected, is already waiting to
-        /// reconnect, or has been permanently shut down, this has no effect.
-        /// </remarks>
-        /// <param name="resetBackoffDelay">true if the delay before reconnection should be reset to
-        /// the lowest level (<see cref="ConfigurationBuilder.InitialRetryDelay(TimeSpan)"/>); false if it
-        /// should increase according to the usual exponential backoff logic</param>
+        /// <inheritdoc/>
         public void Restart(bool resetBackoffDelay)
         {
             lock (this)
