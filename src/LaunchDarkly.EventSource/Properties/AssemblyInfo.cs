aca39a7887c8af9892c57d585a9fe71d7233a4a3
Eli Bishop <eli@launchdarkly.com>
2021-02-22 11:39:01 -0800

    create GitHub Pages branch

--- src/LaunchDarkly.EventSource/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Runtime.CompilerServices;
-
-#if DEBUG
-// Added to allow the Test Project to access internal types and methods.
-[assembly: InternalsVisibleTo("LaunchDarkly.EventSource.Tests")]
-#endif
